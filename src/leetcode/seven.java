b5c69c1f4eff1cf21b8120e4ba8e2db828ae287e
Mac <mac@MacBook-Pro.local>
2019-07-04 17:08:09 +0800

    整数反转

--- src/leetcode/seven.java
+++ src/leetcode/seven.java
@@ -2,8 +2,19 @@ package leetcode;
 
 public class seven {
 
-    public int reverse(int x) {
-
-        return 0;
+    public static void main(String[] args) {
+        System.out.println(reverse(320));
+    }
+    public static int reverse(int x) {
+        int result = 0;
+        while(x!=0){
+           int temp = result*10 + x%10;
+           if (temp/10!=result){
+               return 0;
+           }
+           result = temp;
+           x /= 10;
+        }
+        return result;
     }
 }
