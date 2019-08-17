d5c15218b00bbea388a363d6d6a988cda04e8b13
yunlong.syl <yunlong.syl@alibaba-inc.com>
2019-08-17 17:47:12 +0800

    fix

---
+++ src/codeSeven/TestVolatile.java
@@ -0,0 +1,44 @@
+package codeSeven;
+
+/**
+ * @ClassName TestVolatile
+ * @Decription TODO
+ * @Auther mac
+ * @Date 2019-08-17 14:55
+ * @Version 1.0
+ **/
+public class TestVolatile {
+    private static int x = 0, y = 0;
+    private static int a = 0, b =0;
+
+    public static void main(String[] args) throws InterruptedException {
+        int i = 0;
+        for(;;) {
+            i++;
+            x = 0; y = 0;
+            a = 0; b = 0;
+            Thread one = new Thread(new Runnable() {
+                public void run() {
+                    a = 1;
+                    x = b;
+                }
+            });
+
+            Thread other = new Thread(new Runnable() {
+                public void run() {
+                    b = 1;
+                    y = a;
+                }
+            });
+            one.start();other.start();
+            one.join();other.join();
+            String result = "第" + i + "次 (" + x + "," + y + "）";
+            if(x == 0 && y == 0) {
+                System.err.println(result);
+                break;
+            } else {
+                System.out.println(result);
+            }
+        }
+    }
+}
