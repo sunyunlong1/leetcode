868a91932df96587e74d862ca2eff8511e963e72
yunlong.syl <yunlong.syl@alibaba-inc.com>
2019-12-03 18:56:26 +0800

    merge

---
+++ src/a4/Solution.java
@@ -0,0 +1,46 @@
+package a4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName Solution
+ * @Decription 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+ *
+ * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ *
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ *
+ * 示例 1:
+ *
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ *
+ * 则中位数是 2.0
+ * 示例 2:
+ *
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ *
+ * 则中位数是 (2 + 3)/2 = 2.5
+ *
+ * @Auther mac
+ * @Date 2019-11-11 22:48
+ * @Version 1.0
+ **/
+public class Solution {
+
+    public static void main(String[] args) {
+        Map<String,Object> map = new HashMap<>();
+        System.out.println(map.get(null));
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
+        while(i<nums1.length || j<nums2.length){
+
+        }
+        return 0.0d;
+    }
+}
